8bfa45d9ff03a567135f47bb9bb90d78e7f10dcd
vishveshspal <21f1005309@student.onlinedegree.iitm.ac.in>
2022-07-23 21:40:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+
+
+#heading
+st.write("""# Substraction of two numbers""")
+
+#getting input
+st.header('Number 1 - Number 2')
+
+def substraction_of_Two_numbers():
+  number_1 = st.number_input("Enter Number 1")
+  number_2 = st.number_input("Enter Number 2")
+  result   = (int(number_1) - int(number_2) )
+  return result
+
+st.write("The resultant number is "+ substraction_of_Two_numbers)
+
+
+
